0e8ebc1a8b852b9edeb28b0471e22e78f349f843
Natan Castaneda <ncastanedahidalgo@gmail.com>
2026-05-17 18:24:41 -0600

    First and only commit

---
+++ src/app/gifs/components/gifs-side-menu-options/gifs-side-menu-options.ts
@@ -0,0 +1,31 @@
+import {Component } from '@angular/core';
+import { RouterLink, RouterLinkActive } from "@angular/router";
+
+interface MenuOption {
+  label: string;
+  subLabel: string;
+  route: string;
+  icon: string;
+}
+
+@Component({
+  selector: 'app-gifs-side-menu-options',
+  imports: [RouterLink, RouterLinkActive],
+  templateUrl: './gifs-side-menu-options.html',
+})
+export default class GifsSideMenuOptions {
+  menuOptions: MenuOption[] = [
+    {
+      icon: 'fa-solid fa-chart-line',
+      label: 'Trending',
+      subLabel: 'Gifs populares',
+      route: '/dashboard/trending'
+    },
+    {
+      icon: 'fa-solid fa-magnifying-glass',
+      label: 'Buscador',
+      subLabel: 'Buscar Gifs',
+      route: '/dashboard/search'
+    }
+  ]
+}
